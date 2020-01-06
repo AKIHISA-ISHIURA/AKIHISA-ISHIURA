3bf4329291cafb932198a504ab5ef71e541b63a2
AKIHISA-ISHIURA <u232009a0902a@gmail.com>
2020-01-06 18:17:00 +0900

    コミットメッセージ

---
+++ test/parameter/parameterGet.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>
+<?php echo "Str1は、";?>
+ <font color="red"><?php echo "リンゴ";?></font>
+<?php echo "です。";?>
+</p>
+
+<p>
+<?php echo "Str2は、";?>
+<font color="red"><?php echo "ゴリラ";?></font>
+<?php echo "です。";?>
+</p>
+
+<p>
+<?php echo "Str3は、";?>
+<font color="red"><?php echo "ラッパ";?></font>
+<?php echo "です。";?>
+</p>
+
+</body>
+</html>
