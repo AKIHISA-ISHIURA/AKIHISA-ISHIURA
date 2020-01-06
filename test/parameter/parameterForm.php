9edb86b2532185e3e5642b9c1e0011960e74fc7e
AKIHISA-ISHIURA <u232009a0902a@gmail.com>
2020-01-06 19:19:02 +0900

    コミットメッセージ

---
+++ test/parameter/parameterForm.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="parameterPost.php" method="post">
+Str1: <input type="text" name="いぬ"><br>
+Str2: <input type="text" name="きじ"><br>
+Str3: <input type="text" name="さる"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ test/parameter/parameterPost.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+<p>
+<?php echo "Str1は、";?>
+ <font color="red"><?php echo $_POST["いぬ"]; ?></font>
+<?php echo "です。";?>
+</p>
+
+<p>
+<?php echo "Str2は、";?>
+<font color="red"><?php echo $_POST["きじ"]; ?></font>
+<?php echo "です。";?>
+</p>
+
+<p>
+<?php echo "Str3は、";?>
+<font color="red"><?php echo $_POST["さる"]; ?></font>
+<?php echo "です。";?>
+</p>
+
+</body>
+</html>
