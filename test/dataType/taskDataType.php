f2c7a537a5934d5aad1b2895b4c3612afad39304
AKIHISA-ISHIURA <u232009a0902a@gmail.com>
2020-01-11 17:20:16 +0900

    課題提出 PHP -データ型

---
+++ test/dataType/taskDataType.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskDataType</title>
+</head>
+<body>
+
+    <p><?php echo('リンゴが５個あります。'); ?></p>
+    <p><?php echo('string型を使用するときに、ダブルクォーテーション(")を表示する場合は、バックスラッシュ(\)が必要です。'); ?></p>
+    <p><?php echo('I\'m Security Engineer'); ?></p>
+    <p><?php echo('こんばんは、太郎くん'); ?></p>
+    
+</body>
+</html>
