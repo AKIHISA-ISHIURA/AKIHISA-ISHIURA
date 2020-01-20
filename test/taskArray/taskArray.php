3938ab9d2d67bb51fb29b93b5f02bc83072eaca9
AKIHISA-ISHIURA <u232009a0902a@gmail.com>
2020-01-20 19:59:55 +0900

    課題提出 PHP -配列

--- test/taskArray/taskArray.php
+++ test/taskArray/taskArray.php
@@ -5,32 +5,119 @@
 <html>
 <body>
 
-<?php
-echo "<pre>";
-$array = array( ' 年度 　平均年収', '2018年 4,410,000円', '2017年 4,320,000円', '2016年 4,220,000円', '2015年 4,200,000円', '2014年 4,150,000円' ,);
-var_dump( $array );
-echo "</pre>";
+<?php 
+$value="平均年収";
+$year = "年度";
+var_dump ($year ." ". $value)
 ?>
 
+<p>
 <?php
-echo "<pre>";
-$array = array( 'メンバー 点数', '　 A 　　80', '　 B     55', '   C     61', '   D     76', '   E     34' ,'   F     93' ,);
-var_dump( $array );
-echo "</pre>";
+$A=4410000;
+$year = 2018;
+var_dump ($year ." ". $A)
 ?>
+</p>
+
+<p>
+<?php
+$B=4320000;
+$year = 2017;
+var_dump($year . " ".$B)
+?>
+</p>
+
+<p>
+<?php
+$C=4220000;
+$year = 2016;
+var_dump($year . " ".$C)
+?>
+</p>
+
+<p>
+<?php
+$D=4200000;
+$year = 2015;
+var_dump($year . " ".$D)
+?>
+</p>
+
+<p>
+<?php
+$E=4150000;
+$year = 2014;
+var_dump($year . " ".$E)
+?>
+</p>
 
 <p><?php echo 
-$A = "80";
-$B = "55";
-$C = "61";
-$D = "76";
-$E = "34";
-$F = "93";
 
+$Sum = $A + $B + $C + $D + $E ;
+$Ave = $Sum / 5;
+print ("過去５年の平均年収は:  ".$Ave."円です。");
+ ?></p>
+
+
+<?php 
+$value="メンバー";
+$year = "点数";
+var_dump ($year ." ". $value)
+?>
+
+<p>
+<?php
+$A=80;
+$year = "A";
+var_dump ($year ." ". $A)
+?>
+</p>
+
+<p>
+<?php
+$B=55;
+$year = "B";
+var_dump($year . " ".$B)
+?>
+</p>
+
+<p>
+<?php
+$C=61;
+$year = "C";
+var_dump($year . " ".$C)
+?>
+</p>
+
+<p>
+<?php
+$D=76;
+$year = "D";
+var_dump($year . " ".$D)
+?>
+</p>
+
+<p>
+<?php
+$E=34;
+$year = "E";
+var_dump($year . " ".$E)
+?>
+</p>
+
+<p>
+<?php
+$F=93;
+$year = "F";
+var_dump($year . " ".$F)
+?>
+</p>
+
+<p><?php echo 
 
 $Sum = $A + $B + $C + $D + $E + $F ;
 $Ave = $Sum / 6;
-print("テストの合計: ".$Sum." 点・平均: ".$Ave."点です。");
+print("テストの平均点は:  ".$Ave."点です。");
  ?></p>
 
 </body>
